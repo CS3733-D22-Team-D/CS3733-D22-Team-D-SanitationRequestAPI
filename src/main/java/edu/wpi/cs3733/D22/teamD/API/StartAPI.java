2627fb9c370650286010b1fd4f6110213773c861
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-14 19:09:45 -0400

    JavaDocs for run method

--- src/main/java/edu/wpi/cs3733/D22/teamD/API/StartAPI.java
+++ src/main/java/edu/wpi/cs3733/D22/teamD/API/StartAPI.java
@@ -1,9 +1,22 @@
 package edu.wpi.cs3733.D22.teamD.API;
 
+import edu.wpi.cs3733.D22.teamD.App;
+
 public class StartAPI {
 
     public StartAPI(){}
 
+    /**
+     * Allows for the API to be run when in use by other teams
+     * @param xCoord starting x coordinate of the program window
+     * @param yCoord starting y coordinate of the program window
+     * @param windowWidth width of the program window
+     * @param windowLength height of the program window
+     * @param cssPath path to CSS styling file
+     * @param destLocationID location where the request is done
+     * @param originLocationID location where the service request starts from (IF NEEDED)
+     * @throws ServiceException if something unexpected occurs
+     */
     public static void run(int xCoord, int yCoord, int windowWidth, int windowLength, String cssPath, String destLocationID, String originLocationID) throws ServiceException {
 
     }
