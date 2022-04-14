8f07a68e2c224546ad41d9f4e3e6c601fcfca5f8
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-13 21:11:10 -0400

    Fixed the jacoco errors

--- src/main/java/edu/wpi/cs3733/D22/teamD/controllers/SanitationController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamD/controllers/SanitationController.java
@@ -12,67 +12,44 @@ import javafx.scene.layout.VBox;
 
 public class SanitationController {
 
-    @FXML
-    private TableColumn<?, ?> Assignee;
+  @FXML private TableColumn<?, ?> Assignee;
 
-    @FXML
-    private TableColumn<?, ?> Priority;
+  @FXML private TableColumn<?, ?> Priority;
 
-    @FXML
-    private TableColumn<?, ?> ReqID;
+  @FXML private TableColumn<?, ?> ReqID;
 
-    @FXML
-    private TableColumn<?, ?> Requester;
+  @FXML private TableColumn<?, ?> Requester;
 
-    @FXML
-    private TableColumn<?, ?> RoomID;
+  @FXML private TableColumn<?, ?> RoomID;
 
-    @FXML
-    private TableColumn<?, ?> Service;
+  @FXML private TableColumn<?, ?> Service;
 
-    @FXML
-    private TableColumn<?, ?> Status;
+  @FXML private TableColumn<?, ?> Status;
 
-    @FXML
-    private Button clearButton;
+  @FXML private Button clearButton;
 
-    @FXML
-    private Button csvButton;
+  @FXML private Button csvButton;
 
-    @FXML
-    private JFXComboBox<?> locationBox;
+  @FXML private JFXComboBox<?> locationBox;
 
-    @FXML
-    private TableView<?> pendingRequests;
+  @FXML private TableView<?> pendingRequests;
 
-    @FXML
-    private JFXComboBox<?> priorityBox;
+  @FXML private JFXComboBox<?> priorityBox;
 
-    @FXML
-    private JFXComboBox<?> sanitationBox;
+  @FXML private JFXComboBox<?> sanitationBox;
 
-    @FXML
-    private VBox sceneBox;
+  @FXML private VBox sceneBox;
 
-    @FXML
-    private Button submitButton;
+  @FXML private Button submitButton;
 
-    @FXML
-    private StackPane windowContents;
+  @FXML private StackPane windowContents;
 
-    @FXML
-    void onClearClicked(MouseEvent event) {
+  @FXML
+  void onClearClicked(MouseEvent event) {}
 
-    }
+  @FXML
+  void onSubmitClicked(MouseEvent event) {}
 
-    @FXML
-    void onSubmitClicked(MouseEvent event) {
-
-    }
-
-    @FXML
-    void saveToCSV(ActionEvent event) {
-
-    }
-
+  @FXML
+  void saveToCSV(ActionEvent event) {}
 }
