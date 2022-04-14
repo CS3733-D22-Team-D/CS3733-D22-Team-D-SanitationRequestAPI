8c4f8fb45ac2ff07f076cbb5bfcd61ee6b79fa9d
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-04-14 15:25:13 -0400

    Added Employee DAO to DAOPouch

--- src/main/java/edu/wpi/cs3733/D22/teamD/backend/DAOPouch.java
+++ src/main/java/edu/wpi/cs3733/D22/teamD/backend/DAOPouch.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.D22.teamD.backend;
 
+import edu.wpi.cs3733.D22.teamD.entities.Employee;
 import edu.wpi.cs3733.D22.teamD.entities.Location;
 import edu.wpi.cs3733.D22.teamD.request.SanitationRequest;
 import java.io.IOException;
@@ -8,12 +9,14 @@ import java.sql.SQLException;
 public class DAOPouch {
   private static DAO<SanitationRequest> sanitationRequestDAO;
   private static DAO<Location> locationDAO;
+  private static DAO<Employee> employeeDAO;
 
   private DAOPouch() {}
 
   public static void init() throws SQLException, IOException {
     sanitationRequestDAO = new DAO<>(new SanitationRequest());
     locationDAO = new DAO<>(new Location());
+    employeeDAO = new DAO<>(new Employee());
   }
 
   public static DAO<SanitationRequest> getSanitationRequestDAO() {
@@ -22,5 +25,8 @@ public class DAOPouch {
 
   public static DAO<Location> getLocationDAO() {
     return locationDAO;
+  }
+  public static DAO<Employee> getEmployeeDAO() {
+    return employeeDAO;
   }
 }
