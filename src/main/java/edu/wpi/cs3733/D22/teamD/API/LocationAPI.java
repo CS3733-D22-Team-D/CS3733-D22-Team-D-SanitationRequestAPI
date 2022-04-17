cc52dde34ef615e0566f7bbb82c9d59be1e7f8cd
Samuel Karkache <samkarkachecollege@gmail.com>
2022-04-17 00:49:38 -0400

    moved some code

--- src/main/java/edu/wpi/cs3733/D22/teamD/API/LocationAPI.java
+++ src/main/java/edu/wpi/cs3733/D22/teamD/API/LocationAPI.java
@@ -3,43 +3,36 @@ package edu.wpi.cs3733.D22.teamD.API;
 import edu.wpi.cs3733.D22.teamD.backend.DAO;
 import edu.wpi.cs3733.D22.teamD.backend.DAOPouch;
 import edu.wpi.cs3733.D22.teamD.entities.Location;
-
-import java.io.IOException;
 import java.sql.SQLException;
 
-/**
- * Class that allows users to edit the location database
- */
+/** Class that allows users to edit the location database */
 public class LocationAPI {
 
-    private DAO<Location> locationDAO;
+  private DAO<Location> locationDAO;
 
-    public LocationAPI() {
-        try {
-            DAOPouch.init();
-        } catch (Exception e) {
-            System.err.println("There was an error connecting to the database");
-            throw new RuntimeException();
-        }
-        this.locationDAO = DAOPouch.getLocationDAO();
+  public LocationAPI() {
+    try {
+      DAOPouch.init();
+    } catch (Exception e) {
+      System.err.println("There was an error connecting to the database");
+      throw new RuntimeException();
     }
+    this.locationDAO = DAOPouch.getLocationDAO();
+  }
 
-
-    public void addLocation(Location location) {
-        try {
-            this.locationDAO.add(location);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+  public void addLocation(Location location) {
+    try {
+      this.locationDAO.add(location);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public void removeLocation(Location location) {
-        try {
-            this.locationDAO.delete(location);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+  public void removeLocation(Location location) {
+    try {
+      this.locationDAO.delete(location);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
-
-
+  }
 }
