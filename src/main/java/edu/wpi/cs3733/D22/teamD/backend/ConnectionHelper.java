3ccad80d337eae2f736ac58b44e3eb4105181e67
Samuel Karkache <samkarkachecollege@gmail.com>
2022-04-20 18:47:11 -0400

    added static init back

--- src/main/java/edu/wpi/cs3733/D22/teamD/backend/ConnectionHelper.java
+++ src/main/java/edu/wpi/cs3733/D22/teamD/backend/ConnectionHelper.java
@@ -14,6 +14,12 @@ public class ConnectionHelper {
     CLIENTSERVER
   }
 
+  static {
+    System.out.println("I am running hello!");
+    init();
+    switchToEmbedded();
+  }
+
   private ConnectionHelper() {}
 
   public static void init() {}
